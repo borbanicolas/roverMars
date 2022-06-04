54a6a39cde02529fcb67fc41d3c09b3dc3cda342
Borba <borbanicolasmk@gmail.com>
2022-06-04 18:09:58 -0300

    add edge to the grid

--- roverMars.js
+++ roverMars.js
@@ -88,16 +88,22 @@ class rover {
 
     }
     gridLimit(grid){
-        switch(grid){
-            case grid.indexOf(x) === 21:
-                console.log(grid)
+        let xIsLowerThan0 = grid[0] < 0
+        let yIsLowerThan0 = grid[1] < 0
+        let xIsHigherThan5 = grid[0] > 5
+        let yIsHigherThan5 = grid[1] > 5
+        if(xIsLowerThan0 || yIsLowerThan0 || xIsHigherThan5 || yIsHigherThan5){
+            console.log('vc quebrou a sonda, parabens!')
+            return this.gridPosition = ['broken rover'], this.direction = ''
+        }else{
+            return
         }
     }
 }
 
 
 
-let rover1 = new rover("N", [x = -21, y = 2])
+let rover1 = new rover("N", [x = 1, y = 2])
 let rover2 = new rover ("E", [x = 3, y = 3])
 
 
@@ -105,11 +111,12 @@ comand1 = ['L','M','L','M','L','M','L','M','M']
 comand2 = ['M','M','R','M','M','R','M','R','R','M'] 
 
 
-// console.log(grid)
-// console.log('the rover1 position is in ',rover1.gridPosition, rover1.direction)
-// rover1.instruction(comand1)
+console.log(grid)
+console.log('the rover1 position is in ',rover1.gridPosition, rover1.direction)
+rover1.instruction(comand1)
 rover1.gridLimit(rover1.gridPosition)
-// console.log('the rover2 position is in ',rover2.gridPosition, rover2.direction)
-// rover2.instruction(comand2)
-// console.log('position of rover1 set to ', rover1.gridPosition, rover1.direction)
-// console.log('position of rover2 set to ', rover2.gridPosition, rover2.direction)
+console.log('the rover2 position is in ',rover2.gridPosition, rover2.direction)
+rover2.instruction(comand2)
+rover2.gridLimit(rover2.gridPosition)
+console.log('position of rover1 set to ', rover1.gridPosition, rover1.direction)
+console.log('position of rover2 set to ', rover2.gridPosition, rover2.direction)
